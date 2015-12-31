0b93c0c506ac9412a8796c3cbde700591ff64839
Phil Grunewald <philipp.grunewald@ouce.ox.ac.uk>
2015-12-31 14:21:17 +0000

    using php and sql mostly

--- hhq.php
+++ hhq.php
@@ -10,61 +10,53 @@
 </head>
 
 <body>
-<?php
-// Define connection settings
-//$server = '109.74.196.205';
-$server = 'localhost';
-$dbName = 'Meter';
-$dbUserName = 'phil';
-$dbUserPass = 'SSartori12';
+	<?php
+		// Define connection settings
+		//$server = '109.74.196.205';
+		$server = 'localhost';
+		$dbName = 'Meter';
+		$dbUserName = 'phil';
+		$dbUserPass = 'SSartori12';
+		
+		// Connect to the database server
+		$db = mysqli_connect($server,$dbUserName,$dbUserPass,$dbName);
+		
+		if (mysqli_connect_errno()) {
+		    print '<p class="alert alert-error">Oh, dear. The connect failed: ' . mysqli_connect_error() . '. Please email philipp.grunewald@ouce.ox.ac.uk about it.</p>';
+		    exit();
+		} else {
+			if ($_GET['people'] !== "xx") {
+				$sql="UPDATE Household SET people=$_GET[people] WHERE idHousehold=999";
+				mysqli_query($db, $sql);
+			}
+		}
+		$sql = "SELECT people, income, age_group1, age_group2, age_group3, age_group4 FROM Household WHERE idHousehold = 999";
+		
+		$result = mysqli_query($db, $sql);
+		
+		if (mysqli_num_rows($result) > 0) {
+		    // output data of each row
+		    while($row = mysqli_fetch_assoc($result)) {
+				$people = $row["people"];
+				$age_group1 = $row["age_group1"];
+				$age_group2 = $row["age_group2"];
+				$age_group3 = $row["age_group3"];
+				$age_group4 = $row["age_group4"];
+				$income = $row["income"];
+		    }
+		} else {
+		    echo "0 results";
+		}
+		?>
 
-// Connect to the database server
-$db = mysqli_connect($server,$dbUserName,$dbUserPass,$dbName);
-
-if (mysqli_connect_errno()) {
-    print '<p class="alert alert-error">Oh, dear. The connect failed: ' . mysqli_connect_error() . '. Please email philipp.grunewald@ouce.ox.ac.uk about it.</p>';
-    exit();
-} else {
-
-$sql = "SELECT people, income, age_group3 FROM Household WHERE idHousehold = 999";
-$result = mysqli_query($db, $sql);
-
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-		$people = $row["people"];
-		$age_group1 = $row["age_group1"];
-		$age_group2 = $row["age_group2"];
-		$age_group3 = $row["age_group3"];
-		$income = $row["income"];
-        //echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
-
-}
-
-?>
-
-			<!--
-				if (thisID == 'people') {
-			var thisValue = parseInt(thisButton.innerHTML);
-			-->
     <script type="text/javascript">
 		function radioToggle(thisButton) {
-			var thisID = thisButton.getAttribute('id');
+			//  the ID of the meterButton pressed identifies which "GET" value to set  
 			var thisValue = parseInt(thisButton.value);
-			var hasClass = thisButton.getAttribute('class');
-			var radioButtons = document.getElementsByClassName("meterbutton active");
-				for(var i=0; i<radioButtons.length; i++) {
-				        radioButtons[i].setAttribute('class', 'meterbutton passive');
-				}
-			if (hasClass !== 'meterbutton active') {
-				thisButton.setAttribute('class', 'meterbutton active');
-				var parameter = document.getElementById(thisID);
-				parameter.value = thisValue;
-			}
+			var thisID = thisButton.getAttribute('id');
+			var parameter = document.getElementById(thisID);
+			parameter.value = thisValue;
+	        thisButton.setAttribute('class', 'meterbutton active');
 		};
 		function toggle(button) {
 		  var hasClass = button.getAttribute('class');
@@ -79,10 +71,12 @@ if (mysqli_num_rows($result) > 0) {
 
 	<script type="text/javascript"> <!-- increase number by one -->
 	    function addOne(thisButton) {
-			var counter = parseInt(thisButton.innerHTML);
-	        counter += 1;
-	        thisButton.innerHTML = counter;
-	        thisButton.setAttribute('class', 'meterbutton modified');
+			//  the ID of the meterButton pressed identifies which "GET" value to set  
+			var thisValue = parseInt(thisButton.value);
+			var thisID = thisButton.getAttribute('id');
+			var parameter = document.getElementById(thisID);
+			thisValue += 1;
+			parameter.value = thisValue ;
 	    };
 	    </script>
 	
@@ -168,52 +162,53 @@ if (mysqli_num_rows($result) > 0) {
 		<!--
 	<form method="get" action="submit_hhq.php">
 					-->
+
 <form method="get">
     <div class="carousel-inner">
-					<input type="hidden" id="people" name="people" value="-1">
-					<input type="hidden" id="inc" name="inc" value="-1">
-					<input type="hidden" name="ag1" value="-1">
-					<input type="hidden" name="ag2" value="-1">
-					<input type="hidden" name="ag3" value="-1">
-					<input type="hidden" name="ag4" value="-1">
-					<input type="hidden" name="ag5" value="-1">
-					<input type="hidden" name="ag6" value="-1">
-
+		<input type="hidden" id="people" name="people" value="<?php echo $people; ?>">
+		<input type="hidden" id="inc" name="inc" value="<?php echo $income; ?>">
+		<input type="hidden" id="ag1" name="ag1" value="<?php echo $age_group1; ?>">
+		<input type="hidden" id="ag2" name="ag2" value="<?php echo $age_group2; ?>">
+		<input type="hidden" id="ag3" name="ag3" value="<?php echo $age_group3; ?>">
+		<input type="hidden" id="ag4" name="ag4" value="<?php echo $age_group4; ?>">
+		<input type="hidden" id="ag5" name="ag5" value="<?php echo $age_group5; ?>">
+		<input type="hidden" id="ag6" name="ag6" value="<?php echo $age_group6; ?>">
 		<div class="item active"> <!-- ONE -->
 			<h2> How many people live in your home? </h2>
 				<div class="col-xs-6 top-buffer">
 				<!-- Button 1 -->
-					<button class="meterbutton" id="people" value="1" onClick="radioToggle(this)"> 
+					<button  type="button" class="meterbutton <?php if ($people == 1) {echo "active";} ?>" id="people" value="1" onClick="radioToggle(this)"> 
 					<span class="btnLabel">1</span>
 					<span class="btnCaption"></br>just me</span></button>
 					</div>
+
 				<div class="col-xs-6 top-buffer">
 				<!-- Button 2 -->
-					<button class="meterbutton" id="people" value="2" onClick="radioToggle(this)"> 
+					<button class="meterbutton <?php if ($people == 2) {echo "active";} ?>" id="people" value="2" onClick="radioToggle(this)"> 
 					<span class="btnLabel">2</span>
 					<span class="btnCaption"></br>people</span></button>
 					</div>
 				<div class="col-xs-6 top-buffer">
 				<!-- Button 3 -->
-					<button class="meterbutton" id="people" value="3" onClick="radioToggle(this)"> 
+					<button class="meterbutton <?php if ($people == 3) {echo "active";} ?>" id="people" value="3" onClick="radioToggle(this)"> 
 					<span class="btnLabel">3</span>
 					<span class="btnCaption"></br>people</span></button>
 					</div>
 				<div class="col-xs-6 top-buffer">
 				<!-- Button 4 -->
-					<button class="meterbutton" id="people" value="4" onClick="radioToggle(this)"> 
+					<button class="meterbutton <?php if ($people == 4) {echo "active";} ?>" id="people" value="4" onClick="radioToggle(this)"> 
 					<span class="btnLabel">4</span>
 					<span class="btnCaption"></br>people</span></button>
 					</div>
 				<div class="col-xs-6 top-buffer">
 				<!-- Button 5 -->
-					<button class="meterbutton" id="people" value="5" onClick="radioToggle(this)"> 
+					<button class="meterbutton <?php if ($people == 5) {echo "active";} ?>" id="people" value="5" onClick="radioToggle(this)"> 
 					<span class="btnLabel">5</span>
 					<span class="btnCaption"></br>people</span></button>
 					</div>
 				<div class="col-xs-6 top-buffer">
 				<!-- Button 6 -->
-				<button class="meterbutton" id="people" value="6" onClick="radioToggle(this)">
+					<button class="meterbutton <?php if ($people == 6) {echo "active";} ?>" id="people" value="6" onClick="radioToggle(this)"> 
 					<span class="btnLabel">6</span>
 					<span class="btnCaption"></br>or more</span></button>
 					</div>
@@ -232,12 +227,12 @@ if (mysqli_num_rows($result) > 0) {
      	 	</div>
 
 		<div class="item"> <!-- TWO -->
-			 <h2>How many of them are in each of these age groups?</h2>
+		<h2>Of these <?php echo $people ?> people, how many are in each of these age groups?</h2>
 				<div class="col-xs-6 top-buffer">
 				<!-- Button 1 -->
 					<button class="meterbutton" id="ag1" value="0" onClick="addOne(this)"> 
 					<span class="btnCaption">Under 8</span> </br>
-					<span class="btnLabel-sm">
+					<span class="btnLabel">
 						<?php echo $_GET['ag1']; ?>
 					</span> </button> </div>
 	
@@ -245,32 +240,39 @@ if (mysqli_num_rows($result) > 0) {
 				<!-- Button 2 -->
 					<button class="meterbutton" id="ag2" value="0" onClick="addOne(this)"> 
 					<span class="btnCaption">Age 8 - 18</span> </br>
-					<span class="btnLabel-sm">
-						<?php echo $_GET['ag2']; ?>
-					</span> </button> </div>
+					<span class="btnLabel"> <?php echo $_GET['ag2']; ?> </span> 
+					</button> 
+					</div>
 	
 				<div class="col-xs-6 top-buffer">
 				<!-- Button 3 -->
-					<button class="meterbutton" id="ag3" value="0" onClick="addOne(this)"> 0 </button>
-					<div class="meterbutton caption">Age 19 - 34</div>
+					<button class="meterbutton" id="ag3" value="0" onClick="addOne(this)"> 
+					<span class="btnCaption">Age 19 - 34</span></br>
+					<span class="btnLabel"> <?php echo $_GET['ag3']; ?>
 					</div>
 	
 				<div class="col-xs-6 top-buffer">
 				<!-- Button 4 -->
-				<button class="meterbutton" id="ag4" value="0" onClick="addOne(this)"> <?php echo $age_group3; ?> </button>
-					<div class="meterbutton caption">Age 35 - 50</div>
+					<button class="meterbutton" id="ag4" value="0" onClick="addOne(this)">
+					<span class="btnCaption">Age 35 - 50</span></br>
+					<span class="btnLabel"> <?php echo $_GET['ag4']; ?>
+					</button> 
 					</div>
 	
 				<div class="col-xs-6 top-buffer">
 				<!-- Button 5 -->
-					<button class="meterbutton" id="ag5" value="0" onClick="addOne(this)"> 0 </button>
-					<div class="meterbutton caption">Age 51-70</div>
+					<button class="meterbutton" id="ag5" value="0" onClick="addOne(this)"> 
+					<span class="btnCaption">Age 51-70</span></br>
+					<span class="btnLabel"> <?php echo $_GET['ag5']; ?>
+					</button> 
 					</div>
 	
 				<div class="col-xs-6 top-buffer">
 				<!-- Button 6 -->
-					<button class="meterbutton" id="ag6" value="0" onClick="addOne(this)"> 0 </button>
-					<div class="meterbutton caption">Over 70</div>
+					<button class="meterbutton" id="ag6" value="<?php echo $_GET['ag6']; ?>" onClick="addOne(this)">
+					<span class="btnCaption top">Over 70</span></br>
+					<span class="btnLabel"><?php echo $_GET['ag6']; ?></span>
+					</button> 
 					</div>
 	
 				<div class="col-xs-6 top-buffer">
@@ -364,35 +366,20 @@ if (mysqli_num_rows($result) > 0) {
 					<button class="meterbutton" onClick="radioToggle(this)"> 6 <span class="footnote">or more</span> </button>
 					</div>
 				<div class="col-xs-6 top-buffer">
-				<!-- Button 6 
--->
-						    <input type="hidden" name="TESTa" value="Test A">
-
-						    <input type="hidden" name="TESTb" value="Test B">
+				<!-- Button 6 -->
 <!--
         			<input class="meterbutton active" id="add" name="add" type="submit" value="Finish" />
 -->
 					</div>
      	 		</div>
-
      	 </div>
+		<input class="meterbutton active" id="add" name="add" type="submit" value="Finish" />
     </div><!-- carousel-inner -->
-	 				</form>
+</form>
+					<!--  
+</form>
+-->
 	
-<?php
-$sql="UPDATE Household SET Rooms=$_GET[people] WHERE idHousehold=999";
-
-if (!mysqli_query($db,$sql))
-  {
-  die('I am sorry - something went wrong. Please email philipp.grunewald@ouce.ox.ac.uk.  ' . mysqli_error($db));
-  }
-else
-{
- // header( 'Location: #' ) ;
-}
-mysqli_close($db);
-?>
-
          <a class="left carousel-control" href="#qSlider" data-slide="prev">
           &lsaquo;</a>
          <a class="right carousel-control" href="#qSlider" data-slide="next">
